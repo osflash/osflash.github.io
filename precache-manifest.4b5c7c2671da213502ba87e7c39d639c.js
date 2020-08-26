1869eab85f4bf36c4d69d233201b79546329ce9b
OsFlash <felipeflash10@gmail.com>
2020-08-26 17:56:57 -0300

    Updates

---
+++ precache-manifest.4b5c7c2671da213502ba87e7c39d639c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1759302d94ba5695e5daf46a2c5f2e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039f8fda6ead12337e31",
+    "url": "/static/js/2.6bd7928c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bd7928c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64def079dd95fcc5145d",
+    "url": "/static/js/main.7bafeb0d.chunk.js"
+  },
+  {
+    "revision": "c304920ea151cf988cf3",
+    "url": "/static/js/runtime-main.756c4a76.js"
+  }
+]);
